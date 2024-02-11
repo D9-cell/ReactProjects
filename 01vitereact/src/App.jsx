eb62c586d07b1091d5ed7cbbcecd2a24cba19f0f
DEEPON DAS <deeponsubhro@gmail.com>
2024-02-12 01:21:26 +0530

    Add files via upload

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+      <> 
+    <Chai />
+    <h1>By this we can also render this text.</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>Your Lemon Chai is Ready.</h1>
+    )
+}
+
+export default Chai
