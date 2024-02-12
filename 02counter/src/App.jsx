a6c4684c8f0db93e4c171682d1315cd8f5539700
DEEPON DAS <deeponsubhro@gmail.com>
2024-02-12 19:56:28 +0530

    Update App.jsx

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -13,6 +13,23 @@ function App() {
     counter = counter + 1;
     setCounter(counter)
     console.log("Value Added " , counter);
+    /*
+    setCounter(counter)
+    setCounter(counter)
+    setCounter(counter)
+    setCounter(counter)
+    Above 4 line code will execute the same thing cause every time 1 is passed to setCounter.So the final return value is 2.(if counter = 1)
+    
+     setCounter((prevCounter) => (prevCounter+1))
+     we have a call back function in a setCounter  :  setCounter((lastUpdated State => {performing Action}))
+    
+     setCounter((prevCounter) => (prevCounter+1))
+     setCounter((prevCounter) => (prevCounter+1))
+     setCounter((prevCounter) => (prevCounter+1))
+     setCounter((prevCounter) => (prevCounter+1))
+     Result of Above 4 line Code will give us 5 as a answer when the value/first previous state is 1. 
+    
+    */
   }
 
   const decreaseValue = () => {
